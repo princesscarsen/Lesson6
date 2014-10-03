a90f9800f4d83289cac436c4492032e6bbd33e04
cars5260 <cars5260@ISS147D8-02.scdsb.pri>
2014-10-03 09:17:27 -0400

    Added October 3rd

--- src/MakeEmployees.java
+++ src/MakeEmployees.java
@@ -4,9 +4,41 @@ public class MakeEmployees {
     
     public static void main(String[] args) {
         KeyboardReader k=new KeyboardReader();
-        String name=k.readLine("Enter Your Name: ");
-        System.out.println("Hello " +name);
-        System.out.println("This is another line");
+        Employee emp; //employee
+        String name; //name
+        int type; //type
+        double rate; //hourly pay rate
+        int hours; //hours worked
+        String prompt; //user prompt
+        emp=new Employee();
+        while (true){
+            //get the name and break if blank
+            System.out.println("Enter employee data");
+            name=k.readLine(" Name (or <enter> to quit): ");
+            if(!(emp.setName(name)))break;
+            //get the type until valid
+            while(true) {
+                prompt=" Type ("+emp.getTypeRules()+"): ";
+                type=k.readInt(prompt);
+                if (emp.setType(type))break;
+            }
+            //Get hourly pay rate until valid
+            while(true){
+                prompt= "Hourly rate("+emp.getRateRules()+"): ";
+                rate=k.readDouble(prompt);
+                if(emp.setRate(rate))break;
+            }
+            //get the hours worked until valid
+            while(true){
+                prompt=" Hours("+emp.getHourRules()+"): ";
+                hours=k.readInt(prompt);
+                if(emp.setHours(hours))break;
+            }
+            //print the name and pay
+            System.out.println(" The Weekly pay for ");
+            System.out.println(emp.getName());
+            System.out.println(" is $"+emp.getPay());
+        }
     }
     
 }
